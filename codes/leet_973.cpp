1c11d54c8e515a36b2bdfa4f8ca5685e7787f881
BotBw <wang1570@e.ntu.edu.sg>
2021-12-26 15:36:47 +0800

    daily practice

---
+++ codes/leet_973.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+typedef long long ll;
+#define FOR(i, a, b) for(int i = (a); i <= (b); ++i)
+#define _FOR(i, a, b) for(int i = (a); i >= (b); --i)
+int max(int n,...) {va_list args;va_start(args, n);int ret = INT_MIN;FOR(i, 0, n-1)ret = max(ret, va_arg(args, int));va_end(args);return ret;}
+int min(int n,...) {va_list args;va_start(args, n);int ret = INT_MAX;FOR(i, 0, n-1)ret = min(ret, va_arg(args, int));va_end(args);return ret;}
+#define DEBUG // if debugging codes are contained, delete this line before submitting
+
+class Solution {
+public:
+    vector<vector<int>> kClosest(vector<vector<int>>& points, int K) {
+        priority_queue<vector<int>, vector<vector<int>>, cmp> pq;
+        for (vector<int>& point : points) {
+            pq.push(point);
+            if (pq.size() > K) {
+                pq.pop();
+            }
+        }
+        vector<vector<int>> ans;
+        while (!pq.empty()) {
+            ans.push_back(pq.top());
+            pq.pop();
+        }
+        return ans;
+    }
+private:
+    struct cmp {
+        bool operator()(vector<int>& p, vector<int>& q) {
+            return p[0] * p[0] + p[1] * p[1] < q[0] * q[0] + q[1] * q[1];
+        }
+    };
+};
