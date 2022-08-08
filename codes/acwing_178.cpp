012326195dc7c4476d8433ba1befc1759086ea8c
BotBw <wang1570@e.ntu.edu.sg>
2022-08-08 13:14:50 +0800

    aba

--- codes/acwing_178.cpp
+++ codes/acwing_178.cpp
@@ -1,100 +1,98 @@
 #include <bits/stdc++.h>
+#ifdef DEBUG
+#include "debug.h"
+#else
+#define debug(...) 42
+#define debugif(x,y...) 42
+#endif
 using namespace std;
-typedef long long LL;
-typedef unsigned long long ULL;
-typedef pair<int, int> PII;
-#define FOR(i, a, b) for(int i = (a); i <= (b); ++i)
-#define _FOR(i, a, b) for(int i = (a); i >= (b); --i)
+#define FOR(i, a, b) for(int i = (a); i <= (int)(b); ++i)
+#define _FOR(i, a, b) for(int i = (a); i >= (int)(b); --i)
+#define INT_INF 0x3f3f3f3f
+#define LLONG_INF 0x3f3f3f3f3f3f3f3f
+typedef long long ll;
+typedef pair<int, int> pii;
+typedef pair<ll, ll> pll;
+typedef pair<int, pii> piii;
 
-const int maxe = 1005;
-// const int maxe = 10;
-
-typedef pair<int, int> PII;
-typedef pair<int ,PII> PIII;
-
-int h[maxe], rh[maxe], cnt;
-
+vector<int> h, rh;
 struct edge {
-  int to, w, nxt;
-} e[maxe*maxe*2];
-
-void add(int h[], int a, int b, int w) {
-  e[++cnt] = {b, w, h[a]};
-  h[a] = cnt;
-}
-
-int n, m;
-
+  int v, w, next;
+};
+vector<edge> e;
+vector<int> dis, cnt;
+vector<bool> vis;
 int s, t, k;
 
-int dis[maxe];
-bool vis[maxe];
+struct node {
+  int u, d;
+  bool operator<(const node &b) const {
+    return d + dis[u] > b.d + dis[b.u];
+  }
+};
 
-void dijkstra() {
-  priority_queue<PII, deque<PII>, greater<PII>> q;
-  q.push({0, t});
-  vis[t] = 1;
-  dis[t] = 0;
+void add(vector<int> &hh, int a, int b, int l) {
+  e.push_back({b, l, hh[a]});
+  hh[a] = e.size() - 1;
+}
 
+void dijkstra(int st) {
+  dis[st] = 0;
+  priority_queue<pii, vector<pii>, greater<pii>> q;
+  q.push({0, st});
   while(q.size()) {
-    auto frt = q.top();
+    auto cur = q.top();
+    int u = cur.second;
     q.pop();
-    int x = frt.first;
-    
-    if(vis[x]) continue;
-
-    vis[x] = 1;
-
-    for(int i = rh[x]; i; i = e[i].nxt) {
-      int to = e[i].to, w = e[i].w;
-      if(dis[to] > dis[x] + w) {
-        dis[to] = dis[x] + w;
-        q.push({dis[to], i});
+    if(vis[u]) continue;
+    vis[u] = true;
+    for(int i = rh[u]; ~i; i = e[i].next) {
+      int v = e[i].v;
+      if(dis[v] > dis[u] + e[i].w) {
+        dis[v] = dis[u] + e[i].w;
+        q.push({dis[v], v});
       }
     }
   }
 }
 
-int astar() {
-  priority_queue<PIII, deque<PIII>, greater<PIII>> q;
-
-  q.push({dis[s], {0, s}});
-
-  int cnt = 0;
-
-  if(dis[s] == 0x3f3f3f3f) return -1;
-
+void astar(int st) {
+  priority_queue<node> q;
+  q.push({st, 0});
   while(q.size()) {
-    auto frt = q.top();
-    q.pop();
-    int x = frt.second.second;
-    int dist = frt.second.first;
-    if(x == t) cnt++;
-    if(cnt == k) return dist;
-    for(int i = h[x]; i; i = e[i].nxt) {
-      int b = e[i].to, w = e[i].w;
-      int nxt = dist + w;
-      q.push({nxt + dis[b], {nxt, b}});
+    auto cur = q.top(); q.pop();
+    int u = cur.u;
+    // debug(u);
+    cnt[u]++;
+    if(u == t && cnt[u] == k) {
+      cout << cur.d;
+      return;
+    }
+    for(int i = h[u]; ~i; i = e[i].next) {
+      int v = e[i].v, w = e[i].w;
+      if(cnt[v] <= k) q.push({v, cur.d + w});
     }
   }
-  return -1;
+  cout << -1;
 }
 
-#ifdef LEET
-#include "leet.h"
-#else
 int main() {
+  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  int n, m;
   cin >> n >> m;
+  h = rh = vector<int>(n + 1, -1);
+  dis = vector<int>(n + 1, INT_INF);
+  vis = vector<bool>(n + 1);
+  cnt = vector<int>(n + 1);
   FOR(i, 1, m) {
-    int a,b,l;
-    cin>>a>>b>>l;
+    int a, b, l;
+    cin >> a >> b >> l;
     add(h, a, b, l);
     add(rh, b, a, l);
   }
-  cin>>s>>t>>k;
-  if(s==t) k++;
-  dijkstra();
-  cout << astar();
+  cin >> s >> t >> k;
+  if(s == t) k++;
+  dijkstra(t);
+  astar(s);
   return 0;
 }
-#endif
