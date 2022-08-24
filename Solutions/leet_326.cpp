5682ef1a18f947fc0795090eb4115350938b537b
BotBw <wang1570@e.ntu.edu.sg>
2022-08-24 11:35:50 +0800

    water

---
+++ Solutions/leet_326.cpp
@@ -0,0 +1,19 @@
+#include "bits/stdc++.h"
+
+class Solution {
+public:
+    bool isPowerOfThree(int n) {
+        ll i = 1; 
+        while(i < n) {
+          i *= 3;
+        }
+        return i == n;
+    }
+};
+
+
+int main() {
+  ios::sync_with_stdio(0); cin.tie(0); cout.tie(0);
+  
+  return 0;
+}
